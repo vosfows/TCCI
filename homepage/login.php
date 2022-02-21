ca95752051613392ca6b5e0c4791c8a218e9b7c3
vosfows <73765410+vosfows@users.noreply.github.com>
2022-02-21 10:23:47 +0800

    Create login.php

---
+++ homepage/login.php
@@ -0,0 +1,38 @@
+<?php session_start();
+include('dbconn.php');
+if (count($_POST) > 0) {
+    $result = sqlsrv_query($oConn, "SELECT * FROM accounts WHERE username='" . $_POST["username"] . "' and password = '" . $_POST["password"] . "'");
+    $row  = sqlsrv_fetch_array($result);
+    if (is_array($row)) {
+        $_SESSION["id"] = $row['id'];
+        $_SESSION["name"] = $row['name'];
+    } else {
+        echo "<script type='text/javascript'>alert('You have entered incorrect username or password!');</script>";
+    }
+}
+if (isset($_SESSION["id"])) {
+    header("Location:index.php");
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <p>Login</p>
+    <center>
+        <form action="" method="POST">
+            <input type="text" id="user" name="username" placeholder="username">
+            <br>
+            <input type="text" id="pass" name="password" placeholder="password">
+            <button type="submit" id="btn" name="login" default>Login</button>
+        </form>
+    </center>
+</body>
+
+
+</html>
